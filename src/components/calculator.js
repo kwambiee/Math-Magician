fa332f88e9d8d9e7afcecd45bd70228db73fe0d6
Kwamboka <kwambokaj2.jk@gmail.com>
2022-05-25 17:59:30 +0300

    Add values to input field

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -10,12 +10,19 @@ class Calculator extends React.Component {
       input: '',
     };
     this.handleClick = this.handleClick.bind(this);
+    this.mathOperations = this.mathOperations.bind(this);
   }
 
   handleClick(event) {
-    this.setState({
-      input: event.target.name,
-    });
+    this.setState((state) => ({
+      input: `${state.input}${event.target.name}`,
+    }));
+  }
+
+  mathOperations() {
+    if (this.state.input) {
+      const inputValue = this.state.input;
+    }
   }
 
   render() {
@@ -26,61 +33,66 @@ class Calculator extends React.Component {
             <input type="text" value={this.state.input} />
           </form>
           <div className="buttons">
-            <button type="button" onClick={handleClick}>
+            <button type="button" id="clear" onClick={this.handleClick}>
               AC
             </button>
-            <button type="button" onClick={handleClick}>
+            <button type="button" onClick={this.handleClick}>
               +/-
             </button>
-            <button type="button" onClick={handleClick}>
+            <button type="button" name="%" onClick={this.handleClick}>
               %
             </button>
-            <button type="button" onClick={handleClick}>
+            <button type="button" name="/" onClick={this.handleClick}>
               &divide;
             </button>
-            <button type="button" onClick={handleClick}>
+            <button type="button" name="7" onClick={this.handleClick}>
               7
             </button>
-            <button type="button" onClick={handleClick}>
+            <button type="button" name="8" onClick={this.handleClick}>
               8
             </button>
-            <button type="button" onClick={handleClick}>
+            <button type="button" name="9" onClick={this.handleClick}>
               9
             </button>
-            <button type="button" onClick={handleClick}>
+            <button type="button" name="x" onClick={this.handleClick}>
               x
             </button>
-            <button type="button" onClick={handleClick}>
+            <button type="button" name="4" onClick={this.handleClick}>
               4
             </button>
-            <button type="button" onClick={handleClick}>
+            <button type="button" name="5" onClick={this.handleClick}>
               5
             </button>
-            <button type="button" onClick={handleClick}>
+            <button type="button" name="6" onClick={this.handleClick}>
               6
             </button>
-            <button type="button" onClick={handleClick}>
+            <button type="button" name="-" onClick={this.handleClick}>
               -
             </button>
-            <button type="button" onClick={handleClick}>
+            <button type="button" name="1" onClick={this.handleClick}>
               1
             </button>
-            <button type="button" onClick={handleClick}>
+            <button type="button" name="2" onClick={this.handleClick}>
               2
             </button>
-            <button type="button" onClick={handleClick}>
+            <button type="button" name="3" onClick={this.handleClick}>
               3
             </button>
-            <button type="button" onClick={handleClick}>
+            <button type="button" name="+" onClick={this.handleClick}>
               +
             </button>
-            <button className="zero" onClick={handleClick} type="button">
+            <button
+              className="zero"
+              name="0"
+              onClick={this.handleClick}
+              type="button"
+            >
               0
             </button>
-            <button type="button" onClick={handleClick}>
+            <button type="button" name="." onClick={this.mathOperations}>
               &bull;
             </button>
-            <button type="button" onClick={handleClick}>
+            <button type="button" name="=" onClick={this.handleClick}>
               =
             </button>
           </div>
