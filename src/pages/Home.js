506935303631caa23a38e43e3299ad8f8577e53b
Kwamboka <kwambokaj2.jk@gmail.com>
2022-05-31 15:28:49 +0300

    Add Home page

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to our page!</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci
+        perspiciatis earum voluptatum veritatis quod, magnam, blanditiis nulla
+        architecto corrupti laborum, accusantium repudiandae eaque voluptate
+        provident amet? Temporibus laborum sed velit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ullam
+        voluptas, itaque sit excepturi impedit temporibus odio consectetur
+        dolorum ex possimus doloremque voluptate eius repudiandae dolorem quo
+        reiciendis vel molestias?
+      </p>
+    </div>
+  );
+}
+
+export default Home;
