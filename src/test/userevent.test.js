ca5dcd07e517ab3bd8e925296fad1d2942059445
Kwamboka <kwambokaj2.jk@gmail.com>
2022-06-01 14:14:45 +0300

    Test for user interactions

---
+++ src/test/userevent.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+
+const Button = ({ onClick, children }) => (
+  <button onClick={onClick}>{children}</button>
+);
+
+test('calls onClick prop when clicked', () => {
+  const handleOperator = jest.fn();
+  render(<Button onClick={handleOperator}>+</Button>);
+  fireEvent.click(screen.getByText('+'));
+  expect(handleOperator).toHaveBeenCalledTimes(1);
+});
